c4e38f1566ca078f1d720d62171f5e8c0d204eaa
Kristofferson Culmer <krcd58@mail.missouri.edu>
2025-06-16 11:27:01 -0500

    Added math and string

---
+++ math_interpreter.py
@@ -0,0 +1,27 @@
+
+def main():
+    pass
+    #while True:
+        #prompt the user for an expression
+        #use the split() method to get the parts of the expression
+        #check the lenght of the list returend from .split()
+        #if len(list) not = 3, 
+        #   output Incorrect format message and reprompt(contiue)
+
+        #try
+        #   get X and Y and Z values from the list
+        #   and check if X and Z are integers by converting to int()
+        #except:
+        #   output Error message and reprompt(continue)
+
+        #Check that operator is +, -, *, /
+        #if operator not in [+, -, *, /]
+        #   output some error message
+        #   reprompt the user (continue)
+
+        #Determine the operation to carry out based on the value of the operator
+        #Use if/elif/else block to evaluate the operator and carry out the appropriate operation
+        #Output the answer
+
+
+main()
